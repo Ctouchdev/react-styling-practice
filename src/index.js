b70cb74b13413e8b109d3bca98fa595154b68fca
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-14 08:58:49 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
 //Create a React app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const time = new Date();
+const currentTime = time.getHours();
+console.log(currentTime);
+var greeting;
+
+if (currentTime >= 1 && currentTime <= 12) {
+  greeting = "Good Morning!";
+  ReactDOM.render(
+    <h1 className="heading" style={{ color: "red" }}>
+      {greeting}
+    </h1>,
+    document.getElementById("root")
+  );
+} else if (currentTime > 12 && currentTime < 18) {
+  greeting = "Good Afternoon!";
+  ReactDOM.render(
+    <h1 className="heading" style={{ color: "green" }}>
+      {greeting}
+    </h1>,
+    document.getElementById("root")
+  );
+} else {
+  greeting = "Good Evening!";
+  ReactDOM.render(
+    <h1 className="heading" style={{ color: "blue" }}>
+      {greeting}
+    </h1>,
+    document.getElementById("root")
+  );
+}
+
 //Show a single h1 that says "Good morning" if between midnight and 12PM.
 //or "Good Afternoon" if between 12PM and 6PM.
 //or "Good evening" if between 6PM and midnight.
